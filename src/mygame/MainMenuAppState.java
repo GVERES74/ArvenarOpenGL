060351be78ff2c683ff1befda56a9772fe13f9e1
GVERES74 <TE332168@HU04HNBO0012.hu.tycoelectronics.com>
2021-04-12 17:12:21 +0200

    Models loaded

--- src/mygame/MainMenuAppState.java
+++ src/mygame/MainMenuAppState.java
@@ -14,6 +14,7 @@ import com.jme3.app.state.BaseAppState;
 import com.jme3.audio.AudioNode;
 import com.jme3.effect.ParticleEmitter;
 import com.jme3.effect.ParticleMesh;
+import com.jme3.effect.shapes.EmitterBoxShape;
 import com.jme3.effect.shapes.EmitterSphereShape;
 import com.jme3.font.BitmapText;
 import com.jme3.input.KeyInput;
@@ -53,6 +54,7 @@ public class MainMenuAppState extends BaseAppState{
     private AudioNode mainMenuThemePlayer;
     float screenHeight, screenWidth;
     WaterCreator waterproc = new WaterCreator();
+    BitmapText menuItemText, camPosInfoText;
         
     
     @Override
@@ -71,10 +73,12 @@ public class MainMenuAppState extends BaseAppState{
         loadMainScene();
         createWorldLight();
         loadSceneModels();
-        createSimpleWater(30, 25, -15f, -1f, -12f);
-        createSimpleWater(10, 15, 22f, -0.5f, 5f);
+        createSimpleWater(35, 25, -15f, -1f, -12f);
+        createSimpleWater(10, 20, 22f, -0.5f, 5f);
         createPrecipitation();
         createFirePlace();
+        createCreek();
+        createWaterFall();
         loadMenuMusic();
         
         createMainMenu();
@@ -106,32 +110,39 @@ public class MainMenuAppState extends BaseAppState{
         public void loadSceneModels(){
 
 //            createModel("stump_roundDetailed.glb", 17f, 0.1f, -15f, 0f, 4f);
-            createModel("Models/Tree_Pine/snow_pine_tree.obj", "Models/Tree_Pine/pine_snow.j3m", 35f, 0.1f, -22, 0f, 0.1f);
-            createModel("Models/Tree_Pine/snow_pine_tree.obj", "Models/Tree_Pine/pine_snow.j3m", -8f, 0.0f, -8f, 0f, 0.1f);
-            createModel("Models/Grass/grass.glb", "Models/Grass/grass.j3m", 20f, 0.3f, -15f, 0f, 3f);
-            createModel("Models/Grass/grass_large.glb", "Models/Grass/grass.j3m", 17f, 0.3f, -10f, 0f, 3f);
+            createModel("Models/Tree_Pine/snow_pine_tree.obj", "Models/Tree_Pine/pine_snow_full.j3m", 35f, 0.1f, -22, 0f, 0.1f);
+            createModel("Models/Tree_Pine/snow_pine_tree.obj", "Models/Tree_Pine/pine_snow_half.j3m", -8f, 0.0f, -8f, 0f, 0.1f);
+            createModel("Models/Tree_Pine/snow_pine_tree.obj", "Models/Tree_Pine/pine_snow_none.j3m", -20f, 0.0f, -3f, 0f, 0.1f);
+            createModel("Models/Grass/grass.glb", "Models/Grass/grass.j3m", 19f, 0.0f, -18f, 0f, 3f);
+            createModel("Models/Grass/grass.glb", "Models/Grass/grass.j3m", -5f, 0.0f, 6f, 0f, 3f);
+            createModel("Models/Grass/grass.glb", "Models/Grass/grass.j3m", -5f, 0.0f, -3f, 0f, 3f);
+            createModel("Models/Grass/grass_large.glb", "Models/Grass/grass.j3m", 16f, 0.0f, -10f, 0f, 3f);
             createModel("Models/Bush/bush_round.obj", "Models/Bush/bush_round.j3m", 28f, -0.2f, 5f, 0f, 0.1f);
             
+            createModel("Models/Bridge/bridge_wood.obj", "Models/Bridge/bridge_wood.j3m", 21f, -0.4f, -13f, 0.9f, 4f);
+            createModel("Models/Grass/grass_large.glb", "Models/Grass/grass.j3m", 20f, -0.5f, -15f, 0f, 3f);
+            createModel("Models/Grass/grass_large.glb", "Models/Grass/grass.j3m", 22f, -0.2f, -11f, 0f, 3f);
+            createModel("Models/Stones/stone_largeA.obj", "", 21.5f, -0.65f, -10.5f, -3f, 2f);
             
             createModel("Models/Campfire/campfire_logs.obj", "Models/Campfire/campfire_logs.j3m", 0f, -0.1f, 0f, 0f, 2f);
             createModel("Models/Campfire/campfire_stones.obj", "Models/Campfire/campfire_stones.j3m", 0f, -0.1f, 0f, 0f, 3f);
             createModel("Models/Floorbed/bed_floor.obj", "Models/Floorbed/bed_floor.j3m", 1f, 0.1f, 3f, 0f, 3f);
             
-            createModel("Models/Crate/Crate-04.obj", "Models/Crate/wood_crate.j3m", 25f, 0.0f, -15f, 1f, 2f);
-            createModel("Models/Crate/Crate-01.obj", "Models/Crate/wood_crate.j3m", 25f, 1.05f, -15f, 3f, 2f);
-            createModel("Models/Crate/Crate-02.obj", "Models/Crate/wood_crate.j3m", 22f, 0.0f, -15f, 6f, 2f);
-            createModel("Models/Crate/Crate-03.obj", "Models/Crate/wood_crate.j3m", 23f, 0.0f, -13f, 19f, 2f);
-            createModel("Models/Crate/Crate-05.obj", "Models/Crate/wood_crate.j3m", 20f, 0.0f, -10f, 0f, 2f);
-            createModel("Models/Crate/Crate-05.obj", "Models/Crate/wood_crate.j3m", 22f, 0.3f, -15f, 0f, 3f);
+            createModel("Models/Crate/Crate-04.obj", "Models/Crate/wood_crate.j3m", 33f, 0.0f, -17f, 1f, 2f);
+            createModel("Models/Crate/Crate-01.obj", "Models/Crate/wood_crate.j3m", 33f, 1.05f, -17f, 3f, 2f);
+            createModel("Models/Crate/Crate-02.obj", "Models/Crate/wood_crate.j3m", 30f, 0.0f, -17f, 6f, 2f);
+            createModel("Models/Crate/Crate-03.obj", "Models/Crate/wood_crate.j3m", 31f, 0.0f, -15f, 19f, 2f);
+            createModel("Models/Crate/Crate-05.obj", "Models/Crate/wood_crate.j3m", 20f, 0.0f, -8f, 0f, 2f);
+            createModel("Models/Crate/Crate-05.obj", "Models/Crate/wood_crate.j3m", 30f, 0.3f, -17f, 0f, 3f);
             
-            createModel("Models/Cage/CageBed.j3o", "Models/Cage/cage.j3m", 25f, 0.0f, -23f, 2f, 1f);
+            createModel("Models/Cage/CageBed.j3o", "Models/Cage/cage.j3m", 25f, 0.0f, -24f, 2f, 1f);
 //            createModel("Models/Trunk/trunk.j3o", "", 5f, 0f, 3f, 90f, 1f);
-            createModel("Models/Barrel/mini_wood_barrel.obj", "Models/Barrel/wood_barrel.j3m", 26f, 0.0f, -14f, 2f, 0.02f);
-            createModel("Models/Barrel/mini_wood_barrel.obj", "Models/Barrel/wood_barrel.j3m", 28f, 0.0f, -21f, 2f, 0.02f);
+            createModel("Models/Barrel/mini_wood_barrel.obj", "Models/Barrel/wood_barrel.j3m", 20f, 0.0f, -19f, 2f, 0.02f);
+            createModel("Models/Barrel/mini_wood_barrel.obj", "Models/Barrel/wood_barrel.j3m", 19f, 0.0f, -23f, 2f, 0.02f);
             
             createModel("Models/Stool/DwarfBeerBarrel.j3o", "Models/Stool/stool.j3m", 2f, 0f, -3f, 0f, 0.5f);
             createModel("Models/Tent/tent_detailedOpen.obj", "Models/Tent/tent.j3m", -5f, 0f, 3f, 90f, 6f);
-            createModel("Models/Signpost/sign.obj", "Models/Signpost/signpost.j3m", 25f, -0.5f, -10f, 0f, 5f);
+            createModel("Models/Signpost/sign.obj", "Models/Signpost/signpost.j3m", 21f, -0.25f, -8f, -1f, 3f);
             
 
         }
@@ -197,6 +208,54 @@ public class MainMenuAppState extends BaseAppState{
             
         }
         
+        public void createCreek(){
+            ParticleEmitter creekemitter = new ParticleEmitter("Emitter", ParticleMesh.Type.Triangle, 30);
+            Material waterMaterial = new Material(this.app.getAssetManager(), "Common/MatDefs/Misc/Particle.j3md");
+            waterMaterial.setTexture("Texture", this.app.getAssetManager().loadTexture("Effects/Explosion/flame.png"));
+            creekemitter.setMaterial(waterMaterial);
+            creekemitter.setImagesX(2);
+            creekemitter.setImagesY(2);
+            creekemitter.setEndColor(ColorRGBA.Blue);   
+            creekemitter.setStartColor(ColorRGBA.Cyan); 
+            creekemitter.setLowLife(10f);
+            creekemitter.setHighLife(30f);
+            creekemitter.setStartSize(1.5f);
+            creekemitter.setEndSize(0.1f);
+            creekemitter.setGravity(0, 0, 0);
+            creekemitter.getParticleInfluencer().setInitialVelocity(new Vector3f(-1,-0.1f,-1));
+            creekemitter.getParticleInfluencer().setVelocityVariation(0.1f);
+            creekemitter.setLocalTranslation(32f, -0.7f, -2f);
+            creekemitter.setNumParticles(300);
+            creekemitter.setParticlesPerSec(50);
+            creekemitter.setShape(new EmitterBoxShape(new Vector3f(-2f,-1f,-1f),new Vector3f(2f,1f,1f)));
+            startRootNode.attachChild(creekemitter);
+            
+        }
+        
+        public void createWaterFall(){
+            ParticleEmitter waterfallemitter = new ParticleEmitter("Emitter", ParticleMesh.Type.Triangle, 30);
+            Material waterMaterial = new Material(this.app.getAssetManager(), "Common/MatDefs/Misc/Particle.j3md");
+            waterMaterial.setTexture("Texture", this.app.getAssetManager().loadTexture("Effects/Explosion/flame.png"));
+            waterfallemitter.setMaterial(waterMaterial);
+            waterfallemitter.setImagesX(2);
+            waterfallemitter.setImagesY(2);
+            waterfallemitter.setEndColor(ColorRGBA.Blue);   
+            waterfallemitter.setStartColor(ColorRGBA.Cyan); 
+            waterfallemitter.setLowLife(10f);
+            waterfallemitter.setHighLife(15f);
+            waterfallemitter.setStartSize(1.5f);
+            waterfallemitter.setEndSize(0.1f);
+            waterfallemitter.setGravity(0, 0, 0);
+            waterfallemitter.getParticleInfluencer().setInitialVelocity(new Vector3f(0f,-2f,0f));
+            waterfallemitter.getParticleInfluencer().setVelocityVariation(0.1f);
+            waterfallemitter.setLocalTranslation(32f, 10f, -2f);
+            waterfallemitter.setNumParticles(300);
+            waterfallemitter.setParticlesPerSec(50);
+            waterfallemitter.setShape(new EmitterBoxShape(new Vector3f(-1f,-1f,-1f),new Vector3f(1f,1f,1f)));
+            startRootNode.attachChild(waterfallemitter);
+            
+        }
+        
         public void createSimpleWater(float width, float depth, float posx, float posy, float posz){
             
             SimpleWaterProcessor waterCreator = new SimpleWaterProcessor(this.app.getAssetManager());
@@ -238,11 +297,12 @@ public class MainMenuAppState extends BaseAppState{
         
         
         protected void moveCamera(){
-            float moveSpeed = 0.001f;
+            float moveX = this.app.getCamera().getDirection().x/500;
+            float moveZ = this.app.getCamera().getDirection().z/500;
             float camx = this.app.getCamera().getLocation().x;
             float camz = this.app.getCamera().getLocation().z;
             float camy = this.app.getCamera().getLocation().y;
-              this.app.getCamera().setLocation(new Vector3f(camx*moveSpeed, camy, camz*moveSpeed));
+              this.app.getCamera().setLocation(new Vector3f(camx-moveX, camy, camz-moveZ));
 
         }
              
@@ -256,11 +316,11 @@ public class MainMenuAppState extends BaseAppState{
             createMenuText("Credits", 50, screenHeight-110);
             createMenuText("Exit Game", 50, screenHeight-140);
 
-            createMenuText("CamDir: "+this.app.getCamera().getDirection(), 50, screenHeight-200);
+            
         }
              
         public void createMenuText(String name, float posx, float posy){
-            BitmapText menuItemText = new BitmapText(this.app.getAssetManager().loadFont("Interface/Fonts/Antiqua.fnt"), false);
+            menuItemText = new BitmapText(this.app.getAssetManager().loadFont("Interface/Fonts/Antiqua.fnt"), false);
             menuItemText.setText(name);
             menuItemText.setSize(24);
             menuItemText.setLocalTranslation(posx, posy, 0);
@@ -284,11 +344,10 @@ public class MainMenuAppState extends BaseAppState{
         private final ActionListener actionListener = new ActionListener() {
             @Override
             public void onAction(String name, boolean keyPressed, float tpf) {
-                            if (name.equals("MBLeft")) {
-
-                    createMenuText("CamDir: "+app.getCamera().getDirection(), 100, 200);
-
-                }
+                if (name.equals("MBLeft")) {
+                    createMenuText("CamDir: "+app.getCamera().getLocation(), 50, screenHeight-200);
+                    
+                                    }
             }
         };
     
@@ -296,8 +355,8 @@ public class MainMenuAppState extends BaseAppState{
     @Override
     public void update(float tpf) {
         //TODO: implement behavior during runtime
-//        rotateCamera(1,tpf,Vector3f.UNIT_Y);
-        //moveCamera();
+        rotateCamera(1,tpf,Vector3f.UNIT_Y);
+        moveCamera();
        
     }
     
